e10603dbfc11b3cd46ae7eb18b039cdfdca2e395
Darko-Martinovic <darko.martinovic@outlook.com>
2018-06-15 13:05:17 +0200

    Added an option to iterate through indexes collection.

--- SmoIntroduction/RebuildIndexes.cs
+++ SmoIntroduction/RebuildIndexes.cs
@@ -17,7 +17,14 @@ namespace SmoIntroduction
 
             var connectionString = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
 
+            // change the consts below
+            // fill factor
             const int fillFactor = 100;
+            // how to rebuild when false iterate through indexes collection
+            const bool howToRebuild = true;
+
+            // only if howToRebuild is set to false. 
+            const int numberOfPages = 100;
 
             ServerConnection cnn;
             string databaseName;
@@ -45,27 +52,51 @@ namespace SmoIntroduction
                     continue;
                 if (tbl.IsMemoryOptimized)
                     continue;
-               
-                try
-                {
-                    Console.WriteLine($@"Rebuilding indexes for table : {tbl.Schema}.{tbl.Name}");
-                    //
-                    // DBCC DBREINDEX(N'{0}', N'', {1}) is executed
-                    //
 
-                    tbl.RebuildIndexes(fillFactor);
-                    Console.WriteLine($@"Updating statistics for table : {tbl.Schema}.{tbl.Name}");
-                    //
-                    // "UPDATE STATISTICS <table name> <statistics name> is executed"
-                    //
+                if (howToRebuild)
+                {
+
+
+                    try
+                    {
+                        Console.WriteLine($@"Rebuilding indexes for table : {tbl.Schema}.{tbl.Name}");
+                        //
+                        // DBCC DBREINDEX(N'{0}', N'', {1}) is executed
+                        //
+
+                        tbl.RebuildIndexes(fillFactor);
+                        Console.WriteLine($@"Updating statistics for table : {tbl.Schema}.{tbl.Name}");
+                        //
+                        // "UPDATE STATISTICS <table name> <statistics name> is executed"
+                        //
+                        tbl.UpdateStatistics(StatisticsTarget.Column, StatisticsScanType.FullScan);
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(string.Join(Environment.NewLine + "\t", ex.CollectThemAll(ex1 => ex1.InnerException)
+                            .Select(ex1 => ex1.Message)));
+                        Console.ReadLine();
+                    }
+                }
+                else
+                {
+                    foreach (Index i in tbl.Indexes)
+                    {
+                        // return datatable with information about fragmentation
+                        var frag = i.EnumFragmentation();
+
+                        if ((int)frag.Rows[0]["Pages"] < numberOfPages)
+                            continue;
+
+                        i.FillFactor = fillFactor;
+
+                        //DBCC SHOW_STATISTICS ({0}, {1}) - return dataset with 3 tables
+                        //var ds = i.EnumStatistics(); 
+                        i.Rebuild();
+                    }
                     tbl.UpdateStatistics(StatisticsTarget.Column, StatisticsScanType.FullScan);
 
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(string.Join(Environment.NewLine + "\t", ex.CollectThemAll(ex1 => ex1.InnerException)
-                        .Select(ex1 => ex1.Message)));
-                    Console.ReadLine();
                 }
 
             }
